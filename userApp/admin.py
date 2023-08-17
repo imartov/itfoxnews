73e718292d3ee53eb4b718915f3184c95d3cfbd5
imartov <alexandr.kosyrew@mail.ru>
2023-08-17 23:04:49 +0300

    created custom uthentication

--- userApp/admin.py
+++ userApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "password")
+
+
+admin.site.register(models.User, UserAdmin)
