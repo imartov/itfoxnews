8110c5e49908fa7eb2136368eb3baa53207a985a
imartov <alexandr.kosyrew@mail.ru>
2023-08-27 14:38:11 +0300

    update serializer in newsApp

--- newsApp/serializers.py
+++ newsApp/serializers.py
@@ -6,9 +6,16 @@ from .models import *
 class CommentSerializer(serializers.ModelSerializer):
     newspost = serializers.StringRelatedField(read_only=True)
     author = serializers.ReadOnlyField(source='author.username')
+    # detail_link = serializers.SerializerMethodField()
     class Meta:
         model = Comment
         fields = "__all__"
+
+    # def get_detail_link(self, obj):
+    #     request = self.context.get('request')
+    #     return request.build_absolute_uri(reverse('newsApp:newspost_comment_detail',
+    #                                             kwargs={'newspost_id': obj.newspost_id,
+    #                                             "comment_id": obj.id}))
 
 
 class NewsPostSerializer(serializers.ModelSerializer):
@@ -20,8 +27,10 @@ class NewsPostSerializer(serializers.ModelSerializer):
 
     def get_comments(self, obj):
         comments = Comment.objects.filter(newspost=obj)[:10]
+        comments_count = Comment.objects.filter(newspost=obj).count()
         request = self.context.get('request')
         return {
             "comments": CommentSerializer(comments, many=True).data,
-            "all_comment_link": request.build_absolute_uri(reverse('newsApp:newspost_comment_list', kwargs={'newspost_id': obj.id}))
+            "all_comment_link": request.build_absolute_uri(reverse('newsApp:newspost_comment_list', kwargs={'newspost_id': obj.id})),
+            "comments_count": comments_count
         }
